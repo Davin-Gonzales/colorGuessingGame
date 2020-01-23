f20988f2f64dca073a913912c2b0bab68f98987c
Davin Gonzales <gonzalesyoungda@gmail.com>
2020-01-23 06:22:43 -0700

    returning the first rgb value

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var buttons = document.getElementsByClassName('colorButton');
 var heading;
+var rgbArray = [];
 heading = document.getElementById('colorValue');
+
 
 buttons[0]
 class rgbValues{
@@ -22,6 +24,26 @@ function randomButtonColor(button, redRGB, greenRGB, blueRGB){
                        );
 }
 
+// function displayColorRGB(red, green, blue){
+//     document.getElementById('output').innerHTML(red, green, blue)
+// };
+
+// function toggleColor(){
+//     var r = 255;
+//     var g = 0;
+//     var b = 0;
+//     var colorButton = document.getElementById('colorDisplayButton');
+//     var displaySetting = colorButton.style.display;
+//     if (displaySetting == 'flex'){
+//         colorButton.innerHTML = 'Show Color';
+//         colorButton.style.display = 'inline';
+//     }
+//     else{
+//         colorButton.innerHTML = 'Hide Color';
+//         colorButton.style.display = 'flex';
+//     }
+
+// }
 
 var answerMessage = document.getElementById('answer');
 
@@ -34,10 +56,20 @@ for (var i = 0; i < buttons.length; i++){
     let green = new rgbValues();
     let blue = new rgbValues();
     
+    rgbArray[0] = red.color;
+    rgbArray[1] = green.color;
+    rgbArray[2] = blue.color;
+
+    console.log(rgbArray);
+
     randomButtonColor(buttons[i], red.color, green.color, blue.color);
 
-    
-    
+    function rgbToString(r, g, b){
+        return `${r}, ${g}, ${b} good`;
+    }
+
+    document.getElementById('colorRGB').innerHTML = rgbToString(red.color, green.color, blue.color)
+
 }
 }
 
